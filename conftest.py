c2c0c8756b1dd905c96180e9732bc32c5cc6a076
stupin-max <stupinmax7@gmail.com>
2023-03-30 10:02:05 +0300

    logger v0 has been added

--- conftest.py
+++ conftest.py
@@ -2,24 +2,41 @@ import pytest
 import random
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-# from fake_useragent import UserAgent
+import logging
+
+def log():
+    """ Logger setup"""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(f"out.log", mode='w')
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
 
-m_user_agent = f"Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (KHTML, как Gecko) Chrome / 81.0.4044.92 Safari / 537.36"
-# proxy = ['93.123.226.23:81']
-
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser():
+
+    logger = log()
     #options
+    logger.info(f"Start option preparation")
+
     path_to_driver = Service('./chromedriver.exe')
+    m_user_agent = f"Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (KHTML, как Gecko) Chrome / 81.0.4044.92 Safari / 537.36"
     options = webdriver.ChromeOptions()
-    # user_agent = UserAgent()
-    # options.add_argument(f"user-agent={user_agent.random}")
+
     options.add_argument(f"user-agent={m_user_agent}")
     options.add_argument(f"--disable-blink-features=AutomationControlled")
-    # options.add_argument(f"--proxy-server={random.choice(proxy)}")
+
     #driver
-    driver = webdriver.Chrome(service = path_to_driver, options = options)
-    driver.maximize_window()
+    try:
+        driver = webdriver.Chrome(service = path_to_driver, options = options)
+        driver.maximize_window()
+        logger.info(f"Driver is ready")
+    except Exception as err:
+        logger.error(f"Problem with  driver: {err}")
+    logger.info(f"Start testing")
     yield driver
+    logger.info(f"Tests finished")
     driver.quit()
